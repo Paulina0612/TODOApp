5da62b3bc239c6acf5f520046ab640e32ffb7b4e
Paulina Bartczak <paulinabartczak2@onet.pl>
2026-04-27 13:35:36 +0200

    Tasks visible in the main window.

--- TODOApp/Forms/Main.cs
+++ TODOApp/Forms/Main.cs
@@ -30,15 +30,27 @@ namespace TODOApp.Forms
 
             if (jobject != null)
             {
-                int id = 3;
-                if (jobject[id.ToString()] is not null)
+                for (int id = 0; id < count; id++)
                 {
-                    Task? task = JsonConvert.DeserializeObject<Task>(jobject[id.ToString()].ToString());
-                    MessageBox.Show(task?.Name);
-                }
-                else
-                {
-                    MessageBox.Show("Task with id " + id + " not found.");
+                    if (jobject[id.ToString()] is not null)
+                    {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                        Task? task = JsonConvert.DeserializeObject<Task>(jobject[id.ToString()].ToString());
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                        if (task != null)
+                        {
+#pragma warning disable CS8604 // Possible null reference argument.
+                            this.tasksDataGridView.Rows.Add
+                                (
+                                    task?.Name,
+                                    task?.Description,
+                                    task?.Status,
+                                    task?.Priority,
+                                    task?.DueDate
+                                );
+#pragma warning restore CS8604 // Possible null reference argument.
+                        }
+                    }
                 }
             }
         }
